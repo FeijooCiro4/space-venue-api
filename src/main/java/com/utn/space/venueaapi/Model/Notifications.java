3a5cc39a9ed46ef1eb4a8718c95a2311c9edf696
Federico Huergo <ffhuergo@gmail.com>
2026-05-30 13:26:01 -0300

    agrege atributos basicos

--- src/main/java/com/utn/space/venueaapi/Model/Notifications.java
+++ src/main/java/com/utn/space/venueaapi/Model/Notifications.java
@@ -1,12 +1,12 @@
 package com.utn.space.venueaapi.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -17,4 +17,15 @@ public class Notifications {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id_Notification;
+
+    private LocalDateTime fecha;
+    private String contenido;
+    private Boolean visto=false;
+
+    @ManyToOne
+    @JoinColumn(name = "id_consumer")
+    @JsonIgnore
+    private Consumers consumer;
+
+
 }
--- src/main/java/com/utn/space/venueaapi/Model/Reservations.java
+++ src/main/java/com/utn/space/venueaapi/Model/Reservations.java
@@ -1,4 +1,36 @@
 package com.utn.space.venueaapi.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
 public class Reservations {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_Reservation;
+
+    private LocalDateTime from_date;
+    private LocalDateTime until_date;
+    private Double final_price;
+
+
+    @ManyToOne
+    @JoinColumn(name = "id_space")
+    @JsonIgnore
+    private Spaces space;
+
+    @ManyToOne
+    @JoinColumn(name = "id_consumer")
+    @JsonIgnore
+    private Consumers consumer;
 }
